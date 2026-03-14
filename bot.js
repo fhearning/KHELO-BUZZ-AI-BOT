f74eea145947fecf5939bc703e64fac44403279d
fhearning <mrsjesminshara@gmail.com>
2026-03-15 05:37:52 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,10 @@
 const TelegramBot = require("node-telegram-bot-api");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const bot = new TelegramBot(8631868074:AAFcC5P0Hc1_E3ImaIIIMKITYWY4MotK3hI, { polling: true });
+const bot = new TelegramBot("8631868074:AAFcC5P0Hc1_E3ImaIIIMKITYWY4MotK3hI", { polling: true });
 
-const genAI = new GoogleGenerativeAI(AIzaSyDkZDAWc7BbMeilB-oog5Qqs0QA72w3Vt0);
+const genAI = new GoogleGenerativeAI("AIzaSyDkZDAWc7BbMeilB-oog5Qqs0QA72w3Vt0");
+
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 bot.on("message", async (msg) => {
@@ -12,9 +13,7 @@ if (!msg.text) return;
 
 try {
 
-const result = await model.generateContent({
-contents: [{ role: "user", parts: [{ text: msg.text }] }]
-});
+const result = await model.generateContent(msg.text);
 
 const reply = result.response.text();
 
