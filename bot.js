a3f1b7e59ada91e2afb2dd28ff09ad8f52baace5
fhearning <mrsjesminshara@gmail.com>
2026-03-15 16:54:06 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,43 +1,17 @@
 const express = require("express");
-const fetch = require("node-fetch");
 
 const app = express();
 app.use(express.json());
 
-const TOKEN = process.env.BOT_TOKEN;
-
 app.get("/", (req, res) => {
-  res.send("Bot running");
+  res.send("Server running");
 });
 
-app.post("/bot", async (req, res) => {
+app.post("/bot", (req, res) => {
 
-  res.sendStatus(200);
+  console.log("Webhook received:", req.body);
 
-  try {
-
-    const msg = req.body.message;
-    if (!msg) return;
-
-    const chatId = msg.chat.id;
-    const text = msg.text || "";
-
-    const reply = "Echo: " + text;
-
-    await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        chat_id: chatId,
-        text: reply
-      })
-    });
-
-  } catch (err) {
-    console.log(err);
-  }
+  res.status(200).send("ok");
 
 });
 
