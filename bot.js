c40f8f56240b277328cc89f3df36e19b9ed832a8
fhearning <mrsjesminshara@gmail.com>
2026-03-15 06:01:48 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,19 @@
 const TelegramBot = require("node-telegram-bot-api");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-// Telegram Bot
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
-// Gemini AI
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash-latest"
+  model: "gemini-2.5-pro"
 });
 
-// Message listener
 bot.on("message", async (msg) => {
 
-  const chatId = msg.chat.id;
-
-  // text না থাকলে ignore
   if (!msg.text) return;
 
+  const chatId = msg.chat.id;
   const userText = msg.text;
 
   try {
@@ -33,9 +28,8 @@ bot.on("message", async (msg) => {
 
   } catch (error) {
 
-    console.log("Gemini Error:", error);
-
-    bot.sendMessage(chatId, "AI error");
+    console.log(error);
+    bot.sendMessage(chatId,"AI error");
 
   }
 
